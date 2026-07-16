a31de6da3d18381134d025bef736e8fdc66568b9
Umesh <majjada.umesh.min21@itbhu.ac.in>
2026-07-16 22:43:02 +0530

    ci: add GitHub Actions backend pipeline

--- backend/src/main/java/com/umesh/distributed_notification_service/domain/provider/email/SpringMailEmailProvider.java
+++ backend/src/main/java/com/umesh/distributed_notification_service/domain/provider/email/SpringMailEmailProvider.java
@@ -2,6 +2,8 @@ package com.umesh.distributed_notification_service.domain.provider.email;
 
 import com.umesh.distributed_notification_service.domain.provider.email.dto.EmailRequest;
 import com.umesh.distributed_notification_service.domain.provider.email.exception.EmailDeliveryException;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +12,8 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 
 @Slf4j
 @Component
@@ -21,6 +25,9 @@ public class SpringMailEmailProvider implements EmailProvider {
     private final TemplateEngine templateEngine;
 
     @Override
+    @Bulkhead(name = "emailProvider", fallbackMethod = "sendFallback")
+    @RateLimiter(name = "emailProvider", fallbackMethod = "sendFallback")
+    @CircuitBreaker(name = "emailProvider", fallbackMethod = "sendFallback")
     public void send(EmailRequest request) {
 
         try {
@@ -61,5 +68,19 @@ public class SpringMailEmailProvider implements EmailProvider {
 
         }
 
+    }
+    
+    private void sendFallback(
+            EmailRequest request,
+            Exception ex) {
+
+        log.error(
+                "Circuit Breaker activated. Email provider unavailable. recipient={}, reason={}",
+                request.getTo(),
+                ex.getMessage());
+
+        throw new EmailDeliveryException(
+                "Email provider is currently unavailable.",
+                ex);
     }
 }
